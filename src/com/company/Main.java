2ec120a2c53d343a694e0f66600b6acad41f2677
Kostya <nightness3@inbox.ru>
2021-09-06 21:50:15 +0300

    The first pull in my life.
    =)

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner (System.in);
+	    System.out.print("This is your first expercience in Java and Git, can you say your name: ");
+	    String name = next();
+	    System.out.print("Can you say you university: ");
+	    String university = next();
+	    System.out.println("Ok, I've done it. Your name: " + name + ". And you university is " + university);
+    }
+}
